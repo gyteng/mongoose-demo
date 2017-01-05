506825ce8d7864c167bbe6621d101456d3b52e75
Guyuteng <igyteng@gmail.com>
2017-01-05 14:17:51 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [quickStart](https://github.com/gyteng/mongoose-demo/blob/master/demo/quickStart.js)  
 [array](https://github.com/gyteng/mongoose-demo/blob/master/demo/array.js)  
 [update](https://github.com/gyteng/mongoose-demo/blob/master/demo/update.js)  
+[aggregate](https://github.com/gyteng/mongoose-demo/blob/master/demo/aggregate.js)  
--- demo/update.js
+++ demo/update.js
@@ -26,10 +26,27 @@ Update.findOneAndUpdate({
   foo: 'B',
 }, {
   $push: { // 往数组增加一条，采用$push
-    bar: {
-      a: 'ZZZ', b: 111,
-    }
+    bar: { a: 'ZZZ', b: 111, }
   }
 }, {
   new: true,
+}).then(console.log).catch(console.log);
+
+Update.update({
+  foo: 'B',
+}, {
+  $pull: { // 删除数组里符合条件的条目
+    bar: { b: 111, },
+  }
+}, {
+  new: true,
+}).then(console.log).catch(console.log);
+
+Update.update({
+  foo: 'B',
+  'bar.a': 123
+}, {
+  $set: {
+    'bar.$.a': 234, // 更改数组内部的条目
+  }
 }).then(console.log).catch(console.log);
